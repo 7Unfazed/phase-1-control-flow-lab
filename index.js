35135970746337e7db0bd043457d970912983657
ElvisMoses <elvis.moses@student.moringaschool.com>
2024-03-19 10:56:49 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(somevalue){
   // Write your code here!
+  if (somevalue<= 400) {return 'This one is on me!';}
+  if (400<somevalue && somevalue<2000) {return 'That will be twenty bucks.';}
+  if ( somevalue>2500) {return  'No can do.';}else {return 'I will gladly take your thirty bucks.';}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+        return 'Thank you so much.';
+    case 'not as generous':
+        return 'Thank you.';
+    case 'thanks for everything':
+        return 'Bye.';
+}
 }
